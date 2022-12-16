dfdd4c1c1d909485f8b148074b743606abb6a2e1
Elijah-glitch <62041760+Elijah-glitch@users.noreply.github.com>
2022-12-16 08:01:18 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,10 +2,10 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 export const auth = firebase.initializeApp({
-    apiKey: "AIzaSyDWtv2TgHKv1JKSnEClhKzTLfud-rupsfE",
-    authDomain: "chatpedia-36738.firebaseapp.com",
-    projectId: "chatpedia-36738",
-    storageBucket: "chatpedia-36738.appspot.com",
-    messagingSenderId: "352308318985",
-    appId: "1:352308318985:web:5a9790b184eafd71c4f9d8"
+    apiKey: "AIzaSyA74ZvfzfZVmopLVsbl88Y02CUaXNWGgwk",
+  authDomain: "roblox-29707.firebaseapp.com",
+  projectId: "roblox-29707",
+  storageBucket: "roblox-29707.appspot.com",
+  messagingSenderId: "1025034692104",
+  appId: "1:1025034692104:web:628690bd4a9e9bf83293bd"
   }).auth()
